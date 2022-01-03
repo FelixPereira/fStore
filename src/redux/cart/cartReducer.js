3eb4beaaa4ff29fc0b8960f40607d081f8723268
Félix Pereira <felixpereira538@gmail.com>
2022-01-03 13:43:23 +0200

    Create carat reducer

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,3 +1,19 @@
+import { cartActionTypes } from './cartActionTypes';
+
 const INITIAL_STATE = {
-  
+  display: false,
 }
+
+const cartReducer = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    case cartActionTypes.TOGGLE_DROPDOWN:
+      return {
+        ...state,
+        display: !state.display
+      }
+    default :
+      return state;
+  }
+}
+
+export default cartReducer;
