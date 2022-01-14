417540cf5a9abf3c5c5df5e80c380dbea74b2274
Felix Pereira <felixpereira538@gmail.com>
2022-01-14 22:54:03 +0100

    increase product quantity on cart

--- src/redux/cart/utilities.js
+++ src/redux/cart/utilities.js
@@ -2,14 +2,10 @@ export const addToCart = (cartItems, itemToAdd) => {
   const existingItem = cartItems.find(cartItem => cartItem.id === itemToAdd.id);
 
   if(existingItem) {
-    return (
-      cartItems.map(cartItem => cartItem.id === itemToAdd.id 
-        ? {...cartItem, quantity: cartItem.quantity++}
-        : cartItem)
-    )
+    return cartItems.map(cartItem => cartItem.id === itemToAdd.id 
+        ? {...cartItem, quantity: cartItem.quantity + 1}
+        : cartItem);
   } else {
-      return (
-        [...cartItems, {...itemToAdd, quantity: 1}]
-      )
+      return [...cartItems, {...itemToAdd, quantity: 1}]
   }
 }
